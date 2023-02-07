25486a0e0fa77c29cedbba2a8c560e298f557a53
Joey <seyoung1st@gmail.com>
2023-02-07 22:24:21 +0900

    249.작업에 페이로드 연결하기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,10 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -21,6 +25,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increse by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
