c33fd7e14095c2cfbcf6f6536abdb9103c6332ea
Joey <seyoung1st@gmail.com>
2023-02-12 22:28:08 +0900

    256 다중 슬라이스 작업하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-export const INCREMENT = "increment";
-
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -23,10 +21,31 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
